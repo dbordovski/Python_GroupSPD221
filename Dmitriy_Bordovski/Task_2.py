9250048b223db87b6564790c8e255a4628fdeb8a
dbordovski <131537700+dbordovski@users.noreply.github.com>
2024-03-16 12:29:13 +0300

    Add files via upload

---
+++ Dmitriy_Bordovski/Task_2.py
@@ -0,0 +1,4 @@
+
+name = input("Your name: ")
+age = input("Your age: ")
+print("My name is " + name, "and I'm " + str(age))
---
+++ Dmitriy_Bordovski/Task_4.py
@@ -0,0 +1,7 @@
+x2 = 5
+x1 = 9
+y1 = 4
+y2 = 8
+from math import hypot
+distance = hypot(x2 - x1, y2 - y1)
+print(distance)
